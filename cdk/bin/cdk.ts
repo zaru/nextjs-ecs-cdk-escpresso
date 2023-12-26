7140db3a1dfce08563f06467fd8148a043772763
zaru <zarutofu@gmail.com>
2023-12-26 11:23:01 +0900

    AZを3つ以上指定できるようにする

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -7,5 +7,12 @@ import { Config, stages } from "../lib/utils";
 const app = new cdk.App();
 for (const stage of stages) {
 	const config = new Config(stage);
-	new NextJsStack(app, config.genId(), {});
+	new NextJsStack(app, config.genId(), {
+		// Availability zoneを3つ以上指定するにはアカウントとリージョンを指定する必要がある
+		// この環境変数はAWSプロファイルが設定されている場合は自動でセットされる
+		env: {
+			account: process.env.CDK_DEFAULT_ACCOUNT,
+			region: process.env.CDK_DEFAULT_REGION,
+		},
+	});
 }
